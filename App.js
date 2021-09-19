7770e8c3238be7f4368379544839b8acbce04af7
AhosanuzzamanRoni <86812185+AhosanuzzamanRoni@users.noreply.github.com>
2021-09-19 16:36:46 +0600

    Class components

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card2 from './components/Card2'
+
+ function App() {
+    return (
+        <div>
+            <Card2 name='Card2'/>
+            <Card2/>
+            
+        </div>
+    )
+    }
+    export default App;
---
+++ Card2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Card2 extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.name}</h3>
+                <h3> hello from Card2</h3>
+            </div>
+        )
+    }
+}
+export default Card2;
